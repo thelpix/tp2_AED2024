b30d47308eff21523a8d440ab9106c76292aa314
thelpix <ivan123456274@gmail.com>
2024-12-05 00:38:29 -0300

    Arregle comparadores

--- src/main/java/aed/ComparatorGanancia.java
+++ src/main/java/aed/ComparatorGanancia.java
@@ -1,13 +1,13 @@
 package aed;
 
-public class ComparatorGanancia extends ComparatorInteger implements Comparador<Ciudad>{
+public class ComparatorGanancia implements Comparador<Ciudad>{
     @Override
     public int comparar(Ciudad c1, Ciudad c2){
         if (c1.superavit == c2.superavit){
-            return compare(c2.posicion, c1.posicion);
+            return (c2.posicion).compareTo(c1.posicion);
         }
         else{
-            return compare(c1.superavit, c2.superavit);
+            return (c1.superavit).compareTo(c2.superavit);
         }
     }
 }
--- src/main/java/aed/ComparatorRedituabilidad.java
+++ src/main/java/aed/ComparatorRedituabilidad.java
@@ -2,14 +2,14 @@ package aed;
 
     //t1.ganancia > t2.ganancia
     //t1.ganancia == t2.ganancia ∧ t1.id < t2.id
-public class ComparatorRedituabilidad extends ComparatorInteger implements Comparador<Traslado>{
+public class ComparatorRedituabilidad implements Comparador<Traslado>{
     @Override
     public int comparar(Traslado t1, Traslado t2){
         if(t1.gananciaNeta == t2.gananciaNeta){
-            return compare(t2.timestamp, t1.timestamp);
+            return (t2.timestamp).compareTo(t1.timestamp);
         }
         else{
-            return compare(t1.gananciaNeta, t2.gananciaNeta);
+            return (t1.gananciaNeta).compareTo(t2.gananciaNeta);
         }
     }
 }
