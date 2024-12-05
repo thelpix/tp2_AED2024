31d1b7000f4ca8bd2275112d0c915e6449a0c447
thelpix <ivan123456274@gmail.com>
2024-12-05 00:45:53 -0300

    corregir comparadores

--- src/main/java/aed/ComparatorAntiguedad.java
+++ src/main/java/aed/ComparatorAntiguedad.java
@@ -5,6 +5,6 @@ package aed;
 public class ComparatorAntiguedad implements Comparador<Traslado>{
     @Override
     public int comparar(Traslado t1, Traslado t2){
-        return t2.timestamp.compareTo(t1.timestamp);
+        return (t2.timestamp).compareTo(t1.timestamp);
     }
 }
